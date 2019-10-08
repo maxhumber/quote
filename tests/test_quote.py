0aa61bf58a775d5bde3d033f96914f4eaa2f305e
Max Humber <max.humber@gmail.com>
2019-10-08 14:11:54 -0400

    cli enabled

--- tests/test_quote.py
+++ tests/test_quote.py
@@ -1,10 +1,9 @@
-import json
 from quote import search
 
 def test_search():
     query = 'blake crouch'
     result = search(query, limit=5)
-    one = json.loads(result)[0]
+    one = result[0]
     assert one['author'] == 'Blake Crouch'
     assert one['book']
     assert isinstance(one['quote'], str)
