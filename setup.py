68f171e5bb8a1e6707dd9917da4fd52216e173c3
Max Humber <max.humber@gmail.com>
2024-10-21 20:11:52 -0400

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="quote",
-    version="2.0.4",
+    version="3.0",
     author="Max Humber",
     author_email="max.humber@gmail.com",
     description="A wrapper for the Goodreads Quote API",
